38c87eea9638be2ee382ec852661bc676cba3a22
Quincy <128651922+qoliver1@users.noreply.github.com>
2023-04-09 12:38:43 -0500

    making some adjustments

--- Family Photo Album Project/MainFamilyPhotoAlbumProject/script.js
+++ Family Photo Album Project/MainFamilyPhotoAlbumProject/script.js
@@ -1,5 +1,5 @@
 const myButton = document.getElementById("alreadyHome");
 
 myButton.addEventListener("click", function() {
-  alert("You're Already Home, My Dear.");
+  alert("You're already home my dear.");
 });
